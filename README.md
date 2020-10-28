37d0571291b9ab86524659f22684312dbe38e3d9
Daisuke Suzuki <ibalucky23@gmail.com>
2020-10-28 22:15:03 +0900

    add readme

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
+# UFO Society公式ホームページの管理アプリ
 
-dep
+## 環境構築方法
+  - 言語のバージョン
+    - node: 12.X
+  - 主要ライブラリのバージョン
+    - react-scripts: 3.X
+    - react: 16.X
+    - redux: 4.X
+    - TypeScript: 3.7.2
+    - firebase: 7.X
+
+  - インストール方法  
+    1. リポジトリをクローン  
+    `$ git clone https://github.com/diskszk/ufo-society1974-admin.git`  
+    1. 作業ディレクトリに移動  
+    `$ cd ufo-society1974-admin`
+    1. 必要なライブラリをインストール  
+    `$ npm install`
+    1. デベロップサーバーを起動  
+    `$ npm start`
+  
+  - デプロイ方法
+    - プログラムをビルドする  
+    `$ npm run build`
+    - Firebase hostingにデプロイ  
+    `$ firebase deploy --only hosting:ufo-society-admin`
+
+## コードの概要
+  - 日本のバンド「UFO Society」が制作したWebページの管理機能を持つWebアプリケーション。
+  - Firebaseが提供するAuthentication/Firestoreを用いてユーザーの登録・管理の機能を実装した。
+  - Firebaseが提供するFirestoreを用いて、管理画面からDBにコンテンツを追加し、元のWebサイトから閲覧できるよう実装した。
+
+## 
+deploy
 firebase deploy --only hosting:ufo-society-admin
--- src/components/users/UserTableBody.tsx
+++ src/components/users/UserTableBody.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { push } from 'connected-react-router';
 import { makeStyles } from '@material-ui/core/styles';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -18,12 +16,11 @@ const useStyles = makeStyles({
 
 type Props = {
   rows: User[];
-  onClick: (uid: string, username: string) => void;
+  onClick: (uid: string, username: string, role: string) => void;
 }
 
 const UserTableBody = (props: Props) => {
   const classes = useStyles();
-  const dispatch = useDispatch();
 
   return (
 
@@ -37,7 +34,7 @@ const UserTableBody = (props: Props) => {
           <TableCell>{row.roleName}</TableCell>
           <TableCell
             className={classes.actionBtn}
-            onClick={() => props.onClick(row.uid, row.username)}
+            onClick={() => props.onClick(row.uid, row.username, row.role)}
           >削除</TableCell>
         </TableRow>
       ))}

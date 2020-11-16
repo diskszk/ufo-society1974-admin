ed0a0469d505ae59d459d72b4b9c8336ad9e05a9
diskszk <ibalucky23@gmail.com>
2020-11-16 19:02:50 +0900

    update: update firestore rules

--- src/lib/generateRandomStrings.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.generateRandomStrings = function () {
-    var S = 'abcdefghijklmnopqrstuvzxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var N = 16;
-    return Array.from(crypto.getRandomValues(new Uint32Array(N)))
-        .map(function (n) { return S[n % S.length]; })
-        .join('');
-};

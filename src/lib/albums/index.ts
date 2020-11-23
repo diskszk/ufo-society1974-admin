2279e1c6e29d154e2a789ba630364f3e587c80e1
diskszk <ibalucky23@gmail.com>
2020-11-23 21:32:30 +0900

    update: update publish function

--- src/lib/albums/index.ts
+++ src/lib/albums/index.ts
@@ -1,3 +1,4 @@
 export { deleteAlbumImage } from './deleteAlbumImage';
 export { saveAlbum } from './saveAlbum';
 export { deleteAlbum } from './deleteAlbum';
+export { publishAlbums } from './publishAlbums';

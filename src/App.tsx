825d2ed1503a56498d088f1bf65071e3468bd12d
diskszk <ibalucky23@gmail.com>
2023-06-04 22:21:20 +0900

    refactor/react-queryを使った場合にReact.SuspenseとErrorBoumdaryが働くように設定する

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./assets/styles/style.scss";
 import "./reset.css";
 
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { QueryErrorResetBoundary } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { Provider } from "react-redux";
 import { createStore } from "./store/store";
 
 import { Header } from "./components/header";
 import Routes from "./Routes";
 
 const store = createStore();
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 const App: React.FC = () => {
   return (
     <React.StrictMode>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Header />
-          <main>
-            <Routes />
-          </main>
-        </BrowserRouter>
-      </Provider>
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary
+            onReset={reset}
+            fallbackRender={({ resetErrorBoundary }) => (
+              <div>
+                There was an error!
+                <button onClick={() => resetErrorBoundary()}>Try again</button>
+              </div>
+            )}
+          >
+            <BrowserRouter>
+              <QueryClientProvider client={client}>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Provider store={store}>
+                    <Header />
+                    <main>
+                      <Routes />
+                    </main>
+                  </Provider>
+                </Suspense>
+              </QueryClientProvider>
+            </BrowserRouter>
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
     </React.StrictMode>
   );
 };

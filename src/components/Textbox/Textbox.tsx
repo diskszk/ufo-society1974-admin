8bbafc3ff2fbbf8a659abac468bb65d623c5e49a
diskszk <ibalucky23@gmail.com>
2023-06-10 14:49:30 +0900

    feat: RHFと連携ができるinputフォームをコンポーネントとして切り出す

---
+++ src/components/Textbox/Textbox.tsx
@@ -0,0 +1,24 @@
+import { forwardRef } from "react";
+import { TextFieldProps, TextField } from "@mui/material";
+
+type Props = React.ComponentPropsWithRef<"input"> & TextFieldProps;
+
+/* 
+  react-hook-formと連携できるmuiのinputコンポーネント
+*/
+export const Textbox = forwardRef<HTMLInputElement, Props>(function Textbox(
+  { ...props },
+  ref
+) {
+  return (
+    <TextField
+      sx={{
+        margin: "1.5em 0",
+        width: "80%",
+      }}
+      type="text"
+      {...props}
+      ref={ref}
+    />
+  );
+});
